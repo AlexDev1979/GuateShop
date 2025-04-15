6879ca2b0600bcb07f3a4c25385207298bcb5c8f
AlexDev1979 <wilgar270@hotmail.com>
2025-04-14 20:46:23 -0600

    creacion del primer patron repositorio

---
+++ GuateShop/GuateShop.FrontEnd/Repositories/HttpResponseWrapper.cs
@@ -0,0 +1,44 @@
+﻿using System.Net;
+
+namespace GuateShop.FrontEnd.Repositories
+{
+    public class HttpResponseWrapper<T>
+    {
+        public HttpResponseWrapper(T? response, bool error, HttpResponseMessage httpResponseMessage)
+        {
+            Response = response;
+            Error = error;
+            HttpResponseMessage = httpResponseMessage;
+        }
+
+        public T? Response { get; }
+        public bool Error { get; }
+        public HttpResponseMessage HttpResponseMessage { get; }
+
+        public async Task<string?> GetErrorMessageAsync()
+        {
+            if (!Error)
+            {
+                return null;
+            }
+            var statusCode = HttpResponseMessage.StatusCode;
+            if (statusCode == HttpStatusCode.NotFound)
+            {
+                return "Recurso no encontrado";
+            }
+            if (statusCode == HttpStatusCode.BadRequest)
+            {
+                return await HttpResponseMessage.Content.ReadAsStringAsync();
+            }
+            if (statusCode == HttpStatusCode.Unauthorized)
+            {
+                return "No autorizado";
+            }
+            if (statusCode == HttpStatusCode.Forbidden)
+            {
+                return "Tienes que estar logueado para ejecurtar esta accion";
+            }
+            return "Error inesperado, por favor intenta de nuevo";
+        }
+    }
+}
